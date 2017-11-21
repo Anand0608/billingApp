17d0ae3eef185c80b33c3b6fd094d8f650734759
PrakashA <anand06.apiit@gmail.com>
2017-11-21 18:10:45 +0530

    Initial Billing App

---
+++ billingApp/src/main/java/com/cinfogen/repository/RoleRepository.java
@@ -0,0 +1,8 @@
+package com.cinfogen.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.cinfogen.model.Role;
+
+public interface RoleRepository extends JpaRepository<Role, Long>{
+}
